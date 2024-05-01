5e2bf6b653d320bb3e1ccdda76641eeee2e74ee8
Micah Dixon <66525641+micahldixon@users.noreply.github.com>
2024-05-01 06:52:59 -0500

    Add files via upload

---
+++ webflow-badge.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(function() {
+      const webflowBadge = document.querySelector('.w-webflow-badge'); 
+      if (webflowBadge) {
+        webflowBadge.href = "https://webflow.partnerlinks.io/59hkezc5os29"; // Your Link Here
+      }
+    }, 1000); 
+  });
